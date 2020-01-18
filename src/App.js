08c9f970146f7285a8a3441e9876fbc82f5ae9c6
Sergey Ganin <mcheguevara2@gmail.com>
2020-01-18 20:10:23 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const data = new Array(100).fill("").map((_, i) => i);
 
 function App() {
+  const [state, setState] = React.useState({
+    pointerStart: 0,
+    pointerEnd: 0,
+    pointerStartTime: 0,
+    pointerEndTime: 0,
+    currScrollPosition: 0
+  });
+
+  function handleDragMove({ nativeEvent }) {
+    console.log("handleDragMove", nativeEvent.y);
+    setState({
+      ...state,
+      currScrollPosition:
+        state.currScrollPosition + (state.pointerStart + nativeEvent.y)
+    });
+  }
+
+  function handleDragStart({ nativeEvent }) {
+    console.log("handleDragStart : ", nativeEvent.y);
+    setState({ ...state, pointerStart: nativeEvent.y });
+  }
+  function handleDragEnd(arg) {
+    return function({ nativeEvent }) {
+      console.log(arg + "_handleDragEnd : ", nativeEvent.y);
+      if (nativeEvent.clientY === state.pointerEnd) return;
+      setState({ ...state, pointerEnd: nativeEvent.clientY });
+      console.log({ start: state.pointerStart, end: nativeEvent.y });
+    };
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        onPointerDown={handleDragStart}
+        onPointerMove={handleDragMove}
+        onPointerUp={handleDragEnd("up")}
+        onPointerCancel={handleDragEnd("cancel")}
+        className="roll-wrapper"
+      >
+        <div
+          // style={{ transform: `translateY(${state.currScrollPosition}px)` }}
+          className="roll-transform"
         >
-          Learn React
-        </a>
-      </header>
+          {data.map(el => (
+            <div key={el} className="roll-element">
+              {el}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
